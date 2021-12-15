211ea837c823a478d62979ec5cd8474540f3759a
hussenk <hussen199710@yahoo.com>
2021-12-15 20:00:13 +0200

    add modle

--- myproject/articles/models.py
+++ myproject/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Name(models.Model):
+    content = models.TextField()
+    title = models.TextField()
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return 
+
