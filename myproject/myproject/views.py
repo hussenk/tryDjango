9e72d197d3086c6a6d61d673513543a1c6da912c
hussenk <hussen199710@yahoo.com>
2021-12-16 16:39:11 +0200

    add templates

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 from articles.models import Article
-
+from django.template.loader import render_to_string
 
 
 
 def home(request):
 
     obj = Article.objects.get(id=1)
-    h1 =f"""
-    <h1>{obj.title}</h1>
-    <p>{obj.content}</p>
-    """
-    return HttpResponse(h1)
+
+    doc = {
+        'id':obj.id,
+        'title' : obj.title,
+        'content':obj.content
+    }
+
+    Html = render_to_string('home-view.html',context=doc )
+    return HttpResponse(Html)
